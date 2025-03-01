e8fe0e19d4f0304ea1ffc0c44f3f89dd6b1afa8a
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2025-03-01 15:21:41 +0800

    migrated primary-categories to production

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,10 @@ export default {
 	env: Deno.env.get('ENV_NAME'),
 	strapi3: {
 		token: Deno.env.get('STRAPI_3_TOKEN'),
-		baseUrl: Deno.env.get('STRAPI_3_BASE_URL'),
+		baseUrl: Deno.env.get('STRAPI_3_URL'),
 	},
 	strapi5: {
 		token: Deno.env.get('STRAPI_5_TOKEN'),
-		baseUrl: Deno.env.get('STRAPI_5_BASE_URL'),
+		baseUrl: Deno.env.get('STRAPI_5_URL'),
 	},
 };
